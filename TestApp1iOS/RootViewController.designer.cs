d3bb2287f4684ab3af92719782793f9870e0113c
Greg Kumik <gkumik@hotmail.com>
2016-12-06 19:13:37 +0000

    IOS

---
+++ TestApp1iOS/RootViewController.designer.cs
@@ -0,0 +1,14 @@
+﻿using Foundation;
+using System.CodeDom.Compiler;
+
+namespace TestApp1iOS
+{
+    [Register("RootViewController")]
+    partial class RootViewController
+    {
+        void ReleaseDesignerOutlets()
+        {
+        }
+    }
+}
+
